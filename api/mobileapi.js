aad35b0df9ab53d40692db64e141817ed4ae8378
cesarmediina <cesarmediina@gmail.com>
2025-06-24 23:08:48 -0300

    feat: Adiciona rota para criação de eventos

--- api/mobileapi.js
+++ api/mobileapi.js
@@ -1,17 +1,13 @@
 const express = require('express');
-
 const app = express();
 const locaisRoutes = require('./routes/locais');
+const eventosRoutes = require('./routes/eventos');
 
-// Middleware para o Express entender JSON no corpo das requisições
 app.use(express.json());
 
-// 2. Aqui está a mágica:
-// Dizemos ao Express: "Para qualquer requisição que comece com o caminho '/locais',
-// use o roteador que a gente importou do arquivo locaisRoutes."
 app.use('/locais', locaisRoutes);
+app.use('/eventos', eventosRoutes);
 
-// Configuração da porta, lendo do ambiente que a Railway fornece
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
